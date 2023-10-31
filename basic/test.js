4f0ede3e1b48c6ef11872d803c89c71c85fed498
Mohd Shumayl <119600843+mohdShumayl@users.noreply.github.com>
2023-10-31 12:28:19 +0000

    array

--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,8 @@
-console.log("hi")
+//let myarr = new Array(1,2,4,5)
+let myarr = [1,2,4,5,8,9,12,14,15]
+let myarr2 = [7,3,9]
+//console.log(myarr.concat(myarr2))
+//console.log(myarr.slice(1,4))
+//console.log(myarr)
+console.log(myarr.splice(2,5))
+console.log(myarr)
