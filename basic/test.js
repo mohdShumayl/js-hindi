cceb25ebed30388b1d43eeb7ffa75cbaff38591d
Mohd Shumayl <119600843+mohdShumayl@users.noreply.github.com>
2023-11-01 07:35:46 +0000

    obj

--- basic/test.js
+++ basic/test.js
@@ -2,7 +2,14 @@
 let myarr = [1,2,4,5,8,9,12,14,15]
 let myarr2 = [7,3,9]
 //console.log(myarr.concat(myarr2))
-//console.log(myarr.slice(1,4))
-//console.log(myarr)
-console.log(myarr.splice(2,5))
+console.log(myarr.slice(1,4))
+
+//console.log(myarr.splice(2,1))
 console.log(myarr)
+
+
+//console.log(myarr.splice(2,5))
+//console.log(myarr.unshift(21))
+
+//console.log(myarr.join())
+//console.log(typeof myarr)
