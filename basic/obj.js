b1b841b3cdca156478a694dfba928ebd831dd0f6
Mohd Shumayl <119600843+mohdShumayl@users.noreply.github.com>
2023-11-21 05:53:39 +0000

    obj

--- basic/obj.js
+++ basic/obj.js
@@ -6,19 +6,66 @@ let obj = {
     [sym]: "nf",
     phn: 536457466,
     show: function(){
-        console.log(`my name is ${this.name}`)
+        console.log(`my name is ${this.name},my age is ${this.age}`)
     }
 }
 
+/*
+function handelObj(anyobject){
+    console.log(`${anyobject.show} & my Phn no is ${anyobject.phn}`)
+}
+handelObj(obj)*/
 
-let obj1 = {abc:8, def:4}
+/*
+function handelObj(anyobject){
+    console.log(anyobject.phn)
+}
+handelObj(obj)
+*/
+arrayy = [2,6,968,64,64,9645,4,6,46,4]
+/*function handleArray(anyarray){
+    console.log(anyarray)
+}
+handleArray(arrayy)*/
+
+/*
+function handleArray(anyarray){
+    console.log(anyarray[3],anyarray[6],anyarray[1])
+}
+handleArray(arrayy)*/
+
+/*function calculateNo(num1){
+    console.log(num1)
+}
+calculateNo(4)*/
+
+
+//rest oprator used to get multiple prameter from an func and store it in array 
+/*function calculateNo(...num1){
+    console.log(num1)
+}
+calculateNo(4,5,6,89,42)*/
+
+
+/*function calculateNo(num, ...num1){
+    console.log(num1)
+}
+calculateNo(4,5,6,89,42)*/
+
+
+/*function calculateNo(num, ...num1){
+    console.log(num, num1)
+}
+calculateNo(4,5,6,89,42,"hrh")*/
+
+/*let obj1 = {abc:8, def:4}
 let obj2 = {c:3, d:5}
-let obj3 = {...obj1, ...obj2}
+let obj3 = {...obj1, ...obj2}*/  //spread oprator used to spread an values of Object and store it in another
+
 //console.log(obj3)
 //console.log(Object.keys(obj3))
 //console.log(Object.values(obj3))
 //console.log(Object.entries(obj3))
-
 
 //object Destructure
 /*const{abc}=obj1
@@ -26,7 +73,6 @@ console.log(abc)*/
 
 /*const{abc:a}=obj1
 console.log(a)*/
-
 
 /*
 let obj1 = {
@@ -42,7 +88,6 @@ Object.assign(obj1, obj2)
 console.log(obj1)
 */
 
-
 /*
 obj.myage = function(){
     //console.log(`my age is ${this.age}`)
@@ -51,14 +96,8 @@ obj.myage = function(){
 obj.myage()
 */
 
-
-
-
-
-
 //obj.name = "Mohammad"
 //console.log(obj["name"])
-
 
 //console.log(obj["name"])
 //console.log(obj.show())
