6c77137658c8d2df6c21f91ed79fe83c7525ac19
Mohd Shumayl <119600843+mohdShumayl@users.noreply.github.com>
2023-11-21 07:18:51 +0000

    func

--- basic/func.js
+++ basic/func.js
@@ -3,7 +3,29 @@
 //when this keyword used directly (without putting it inside any object or function), it returns an empty object in Node Environment, while In browser it returns an object with multiple key values pairs named as Window.
 //thatswhy window object is most global object.(which can lead us to capture all events of window like click,form submit)
 
-function practice(name){
+/*function practice(name){
+  console.log(name)
+}*/
+
+/*const practice = function(name){
     console.log(name)
+}*/
+/*const practice = (name) => {
+    console.log(name)
+}*/
+//practice("hello")
+
+
+//Explesit Return function
+/*const add = (num1, num2) => {
+    console.log(num1 + num2)
 }
-practice("hello")
+add(2,3)*/
+
+//Implesit Return Function
+/*const addition = (num1, num2) => console.log(num1 + num2)
+//OR
+//we can wrap it in parenthesis
+const addition = (num1, num2) => (console.log(num1 + num2))
+
+addition(5,9)*/
